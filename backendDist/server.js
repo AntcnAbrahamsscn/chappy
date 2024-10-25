00f112ed3ba68b48512baa4e3b38ca86a69206a2
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-10-25 17:09:58 +0200

    merge conflict

--- backendDist/server.js
+++ backendDist/server.js
@@ -1,30 +1,15 @@
 import express from "express";
-<<<<<<< HEAD
 import { router as userRouter } from "./routers/usersRouter.js";
-import { router as messagesRouter } from "./routers/messagesRouter.js";
-=======
-import { router as userRouter } from "./routers/userRouter.js";
-import { router as channelRouter } from "./routers/channelsRouter.js";
-import { router as messageRouter } from "./routers/messageRouter.js";
->>>>>>> dev
+import { router as messagesRouter } from "./routers/directMessagesRouter.js";
 const app = express();
 const port = Number(process.env.PORT || 9872);
 // middleware
 app.use("/", express.static("./dist/"));
 app.use("/", express.json());
 // router middleware
-<<<<<<< HEAD
 app.use("/api/users", userRouter);
 // message router
-app.use("/api/messages", messagesRouter);
-=======
-app.use("/api/user", userRouter);
-app.use("/api/channel", channelRouter);
-app.use("/api/message", messageRouter);
->>>>>>> dev
-// routes
-// Route handlers
-// Start
+app.use("/api/direct-messages", messagesRouter);
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
 });
--- backendSrc/server.ts
+++ backendSrc/server.ts
@@ -1,12 +1,7 @@
 import express, { Express } from "express";
-<<<<<<< HEAD
 import { router as userRouter } from "./routers/usersRouter.js";
-import { router as messagesRouter } from "./routers/directMessagesRouter.js";
-=======
-import { router as userRouter } from "./routers/userRouter.js";
 import { router as channelRouter } from "./routers/channelsRouter.js";
 import { router as messageRouter } from "./routers/messageRouter.js";
->>>>>>> dev
 
 const app: Express = express();
 const port: number = Number(process.env.PORT || 9872);
@@ -16,16 +11,9 @@ app.use("/", express.static("./dist/"));
 app.use("/", express.json());
 
 // router middleware
-<<<<<<< HEAD
-app.use("/api/users", userRouter);
-
-// message router
-app.use("/api/direct-messages", messagesRouter);
-=======
 app.use("/api/user", userRouter);
 app.use("/api/channel", channelRouter);
 app.use("/api/message", messageRouter);
->>>>>>> dev
 
 // routes
 // Route handlers
